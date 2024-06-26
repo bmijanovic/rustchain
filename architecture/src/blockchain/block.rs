246ec315a76db7f610fd6a88fc0ad3713d763210
mrmijan <mijanovic.boki.bojan@gmail.com>
2024-06-26 13:55:53 +0200

    added initial mining and hashing

--- architecture/src/blockchain/block.rs
+++ architecture/src/blockchain/block.rs
@@ -1,8 +1,12 @@
 use std::fmt;
+use chrono::{Local, DateTime, Utc};
+use serde_json::{json, Value};
+use crate::utils::utils::crypto_hash;
+use serde::{Serialize, Deserialize};
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Block {
-    pub timestamp: u64,
+    pub timestamp: DateTime<Utc>,
     pub last_hash: String,
     pub hash: String,
     pub data: String,
@@ -11,7 +15,7 @@ pub struct Block {
 }
 
 impl Block {
-    pub fn new(timestamp: u64, last_hash: String, hash: String, data: String, nonce: u64, difficulty: u64) -> Block {
+    pub fn new(timestamp: DateTime<Utc>, last_hash: String, hash: String, data: String, nonce: u64, difficulty: u64) -> Block {
         Block {
             timestamp,
             last_hash,
@@ -24,7 +28,7 @@ impl Block {
 
     pub fn genesis() -> Block {
         Block {
-            timestamp: 0,
+            timestamp: Local::now().with_timezone(&Utc),
             last_hash: "genesis_last_hash".to_string(),
             hash: "genesis_hash".to_string(),
             data: "genesis_data".to_string(),
@@ -32,11 +36,34 @@ impl Block {
             difficulty: 0,
         }
     }
+
+    pub fn mine_block(last_block: &Block, data: &String) -> Block {
+        let timestamp = Local::now().with_timezone(&Utc);
+        let last_hash = last_block.hash.clone();
+        // make hash from all the block properties and nonce
+        let hash = crypto_hash(&[
+            json!(&timestamp),
+            json!(&last_hash),
+            json!(&data)
+        ]);
+        Block {
+            timestamp,
+            last_hash,
+            hash,
+            data: data.clone(),
+            nonce: 0,
+            difficulty: 0,
+        }
+
+    }
 }
 
 impl fmt::Display for Block {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Block - Timestamp: {}, Last Hash: {}, Hash: {}, Data: {}, Nonce: {}, Difficulty: {}", self.timestamp, self.last_hash, self.hash, self.data, self.nonce, self.difficulty)
+        let last_hash = &self.last_hash[..10];
+        let hash = &self.hash[..10];
+        write!(f, "Block - \n    Timestamp: {}, \n    Last Hash: {}, \n    Hash: {}, \n    Data: {}, \n    Nonce: {}, \n    Difficulty: {}"
+               , self.timestamp, last_hash, hash, self.data, self.nonce, self.difficulty)
     }
 }
 
--- architecture/src/blockchain_test/block_test.rs
+++ architecture/src/blockchain_test/block_test.rs
@@ -1,9 +1,11 @@
 use crate::blockchain::block::Block;
+use chrono::{Local, DateTime, Utc};
 
 #[test]
 fn test_block() {
-    let block = Block::new(1, "last_hash".to_string(), "hash".to_string(), "data".to_string(), 1, 1);
-    assert_eq!(block.timestamp, 1);
+    let timestamp = Local::now().with_timezone(&Utc);
+    let block = Block::new(timestamp, "last_hash".to_string(), "hash".to_string(), "data".to_string(), 1, 1);
+    assert_eq!(block.timestamp, timestamp);
     assert_eq!(block.last_hash, "last_hash");
     assert_eq!(block.hash, "hash");
     assert_eq!(block.data, "data");
@@ -14,7 +16,6 @@ fn test_block() {
 
 #[test]
 fn test_genesis_block() {
-    let timestamp = 0;
     let last_hash = String::from("genesis_last_hash");
     let hash = String::from("genesis_hash");
     let data = String::from("genesis_data");
@@ -24,10 +25,18 @@ fn test_genesis_block() {
     let genesis_block = Block::genesis();
 
     // Validating genesis block properties
-    assert_eq!(genesis_block.timestamp, timestamp);
     assert_eq!(genesis_block.last_hash, last_hash);
     assert_eq!(genesis_block.hash, hash);
     assert_eq!(genesis_block.data, data);
     assert_eq!(genesis_block.nonce, nonce);
     assert_eq!(genesis_block.difficulty, difficulty);
+}
+
+#[test]
+fn test_mine_block() {
+    let last_block = Block::genesis();
+    let data = String::from("mined data");
+
+    let mined_block = Block::mine_block(&last_block, &data);
+    print!("{}", mined_block)
 }
